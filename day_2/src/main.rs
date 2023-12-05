426565ff307188ddcc8d51b4200284b62bb24e2e
NicholasJUhlhorn <nick.uhlhorn@gmail.com>
2023-12-05 00:49:27 -0800

    started day two project

---
+++ day_2/src/main.rs
@@ -0,0 +1,11 @@
+// Nicholas J Uhlhorn
+// Advent of Code
+// Day 2 - Bags
+// Input -> List of Games and blocks shown
+//          e.g.: Game <ID>: <# color>, <#, color>; <#, color> ... etc
+// Output <- sum of game record ids that are possible games
+//           if there were 12 red, 13 green, and 14 blue cubes
+
+fn main() {
+    
+}
